7ff942b7f918d2e893a2672095c0f33bc775f9a2
Guillaume Dorce <polynux.dev@gmail.com>
2021-11-14 19:26:36 +0100

    fix main script

--- arch.sh
+++ arch.sh
@@ -3,4 +3,4 @@ bash pre-install.sh
 arch-chroot /mnt /root/arch-scripts/install.sh
 source /mnt/root/arch-scripts/install.conf
 arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/arch-scripts/user.sh
-arch-chroot /mnt /root/arch-scripts/3-post-setup.sh
+arch-chroot /mnt /root/arch-scripts/post-install.sh
