834152a1157d839e9046c9d0d4ae554e40a4cfb2
Guillaume Dorce <polynux.dev@gmail.com>
2021-11-14 18:53:17 +0100

    post install script

---
+++ post-install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#-----------------------------------------------------------------
+#  ██▓███   ▒█████   ██▓   ▓██   ██▓ ███▄    █  █    ██ ▒██   ██▒
+# ▓██░  ██▒▒██▒  ██▒▓██▒    ▒██  ██▒ ██ ▀█   █  ██  ▓██▒▒▒ █ █ ▒░
+# ▓██░ ██▓▒▒██░  ██▒▒██░     ▒██ ██░▓██  ▀█ ██▒▓██  ▒██░░░  █   ░
+# ▒██▄█▓▒ ▒▒██   ██░▒██░     ░ ▐██▓░▓██▒  ▐▌██▒▓▓█  ░██░ ░ █ █ ▒ 
+# ▒██▒ ░  ░░ ████▓▒░░██████▒ ░ ██▒▓░▒██░   ▓██░▒▒█████▓ ▒██▒ ▒██▒
+# ▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▒░▓  ░  ██▒▒▒ ░ ▒░   ▒ ▒ ░▒▓▒ ▒ ▒ ▒▒ ░ ░▓ ░
+# ░▒ ░       ░ ▒ ▒░ ░ ░ ▒  ░▓██ ░▒░ ░ ░░   ░ ▒░░░▒░ ░ ░ ░░   ░▒ ░
+# ░░       ░ ░ ░ ▒    ░ ░   ▒ ▒ ░░     ░   ░ ░  ░░░ ░ ░  ░    ░  
+#              ░ ░      ░  ░░ ░              ░    ░      ░    ░  
+#                           ░ ░                                  
+#-----------------------------------------------------------------
+echo -e "\nFINAL SETUP AND CONFIGURATION"
+echo "--------------------------------------"
+echo "-- GRUB EFI Bootloader Install&Check--"
+echo "--------------------------------------"
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo -e "\nEnabling Login Display Manager"
+systemctl enable sddm.service
+echo -e "\nSetup SDDM Theme"
+cat <<EOF > /etc/sddm.conf
+[Theme]
+Current=Nordic
+EOF
+
+echo -e "\nEnabling essential services"
+systemctl enable NetworkManager.service
+
+echo "
+###############################################################################
+# Cleaning
+###############################################################################
+"
+# Remove no password sudo rights
+sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+# Add sudo rights
+sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+# Replace in the same state
+cd $pwd
+echo "
+###############################################################################
+# Done - Please Eject Install Media and Reboot
+###############################################################################
+"
