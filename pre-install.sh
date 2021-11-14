3c2a9538fe867dd7b77a3e0e7877db4282f35fcf
Guillaume Dorce <polynux.dev@gmail.com>
2021-11-14 15:55:25 +0100

    erase disk

--- pre-install.sh
+++ pre-install.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-
+#-----------------------------------------------------------------
+#  ██▓███   ▒█████   ██▓   ▓██   ██▓ ███▄    █  █    ██ ▒██   ██▒
+# ▓██░  ██▒▒██▒  ██▒▓██▒    ▒██  ██▒ ██ ▀█   █  ██  ▓██▒▒▒ █ █ ▒░
+# ▓██░ ██▓▒▒██░  ██▒▒██░     ▒██ ██░▓██  ▀█ ██▒▓██  ▒██░░░  █   ░
+# ▒██▄█▓▒ ▒▒██   ██░▒██░     ░ ▐██▓░▓██▒  ▐▌██▒▓▓█  ░██░ ░ █ █ ▒ 
+# ▒██▒ ░  ░░ ████▓▒░░██████▒ ░ ██▒▓░▒██░   ▓██░▒▒█████▓ ▒██▒ ▒██▒
+# ▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▒░▓  ░  ██▒▒▒ ░ ▒░   ▒ ▒ ░▒▓▒ ▒ ▒ ▒▒ ░ ░▓ ░
+# ░▒ ░       ░ ▒ ▒░ ░ ░ ▒  ░▓██ ░▒░ ░ ░░   ░ ▒░░░▒░ ░ ░ ░░   ░▒ ░
+# ░░       ░ ░ ░ ▒    ░ ░   ▒ ▒ ░░     ░   ░ ░  ░░░ ░ ░  ░    ░  
+#              ░ ░      ░  ░░ ░              ░    ░      ░    ░  
+#                           ░ ░                                  
+#-----------------------------------------------------------------
 # Color code
 purple="\e[35m";
 reset="\e[0m";
@@ -35,7 +46,22 @@ read -p "Are you sure you want to continue (Y/N):" formatdisk
 case $formatdisk in
 y|yes|YES|Yes|Y)
 
-echo format
+echo "What size of your root partition do you want? (ex: 10G)"
+read SIZE
+
+echo "--------------------------------------"
+echo -e "       Formatting disk..."
+echo "--------------------------------------"
+
+# prep disk
+sgdisk -Z $DISK # erase partition table
+sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+
+#create partitions
+sgdisk -n 1::100M --typecode=1:ef00 --change-name=1:"EFI" $DISK # EFI partition
+sgdisk -n 2::$SIZE --typecode=2:8304 --change-name=2:"root" $DISK # root partition
+sgdisk -n 3::-0 --typecode=3:8302 --change-name=3:"home" $DISK # home partition
+
 
 esac
 esac
