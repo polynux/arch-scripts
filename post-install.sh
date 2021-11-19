4506aa727e155000a0e1ae3a16a49bfdd8fe4427
Guillaume Dorce <polynux.dev@gmail.com>
2021-11-19 23:53:28 +0100

    configure lightdm

--- post-install.sh
+++ post-install.sh
@@ -19,12 +19,12 @@ grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB 
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo -e "\nEnabling Login Display Manager"
-systemctl enable sddm.service
-echo -e "\nSetup SDDM Theme"
-cat <<EOF > /etc/sddm.conf
-[Theme]
-Current=Nordic
-EOF
+systemctl enable lightdm.service
+echo -e "\nSetup LightDM Theme"
+sed -i "s,#greeter-session=,greeter-session=lightdm-webkit2-greeter,g" /etc/lightdm/lightdm.conf
+sed -i "s,#greeter-setup-script=,greeter-setup-script=/usr/bin/numlockx on,g" /etc/lightdm/lightdm.conf
+sed -i "s,antergos,litarvan,g" /etc/lightdm/lightdm-webkit2-greeter.conf
+
 
 echo -e "\nEnabling essential services"
 systemctl enable NetworkManager.service
