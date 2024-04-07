0c1277f66661feb1867859a1d07cf7cb37f930ba
Guillaume Dorce <polynux.dev@gmail.com>
2024-04-07 18:34:59 +0200

    add arco repos

--- arch.sh
+++ arch.sh
@@ -4,3 +4,4 @@ arch-chroot /mnt /root/arch-scripts/install.sh
 source /mnt/root/arch-scripts/install.conf
 arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/arch-scripts/user.sh
 arch-chroot /mnt /root/arch-scripts/post-install.sh
+arch-chroot /mnt /root/arch-scripts/arcorepos.sh
